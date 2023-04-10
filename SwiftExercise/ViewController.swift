64dbfdb1021ec05870d9e356282d738e774d1107
kiyoung92 <go3go1go2@gmail.com>
2023-04-11 00:11:16 +0900

    Initial Commit

---
+++ SwiftExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftExercise
+//
+//  Created by 김기영 on 2023/04/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
