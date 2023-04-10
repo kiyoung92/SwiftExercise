6186f9cd510220096ebd91192d5450301a0c3021
kiyoung92 <go3go1go2@gmail.com>
2023-04-11 00:49:06 +0900

    변수, 상수, 데이터 타입

--- Playground/1. 변수, 상수, 데이터 타입.playground/Contents.swift
+++ Playground/1. 변수, 상수, 데이터 타입.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var str1: String = "hello world"
+let str2: String = "hello world"
+
+str1 = "My Playground"
+//str2 = "My Playground" // 상수 변경 불가
+
+var stringData: String = "문자열"
+var num: Int = 0 // 정수
+var double: Double = 1.2 // 소수점 이하 15자리
+var float: Float = 1.2 // 소수점 이하 6자리
+var boolean: Bool = true // true or false
+var character: Character = "A" // 16비트 유니코드 문자열
+// bit 크기 별 Int, UInt도 있음
+
+var string = "문자열" // 타입 추론 가능
+
+typealias myType = String // 타입 별칭 가능
+
+var numberString: String = "1"
+var numberToString = Int(numberString) // 타입 변환 가능
+
+
