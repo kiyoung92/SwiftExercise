f4b588cc05a03dcda5e8a3b97324d6567467e52e
kiyoung92 <go3go1go2@gmail.com>
2023-04-16 01:02:41 +0900

    삼항연산자, 범위연산자 추가

---
+++ Playground/5. 삼항연산자와 범위연산자.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// 삼항연산자
+let checkbox: Bool = false
+let isChecked: String = checkbox ? "Y": "N"
+
+let age: Int = 32
+let checkAge: String = 30...39 ~= age ? "30대입니다." : "30대가 아닙니다."
+
+// 범위 연산자, 패턴 매칭 연산자
+// Closed Range Operater
+// 1...10
+// 1...
+//  ...10
+
+// Half-Open Range Operator
+// 1 ..< 10
+//   ..< 10
+
+let num: Int = 11
+1...10 ~= num
+1... ~= num
+...10 ~= num
+
+1 ..< 10 ~= num
+..<10 ~= num
