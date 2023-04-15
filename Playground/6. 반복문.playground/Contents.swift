c4f8a97060655821c3ffd636de95da6ba0a289dc
kiyoung92 <go3go1go2@gmail.com>
2023-04-16 01:37:34 +0900

    반복문 추가

---
+++ Playground/6. 반복문.playground/Contents.swift
@@ -0,0 +1,77 @@
+import UIKit
+
+// for
+for i in 1...10 {
+    print(i)
+}
+
+// 와일드 카드 패턴
+for _ in 1...10 {
+    print("helloWorld")
+}
+
+for i in (1...10).reversed() {
+    print(i)
+}
+
+for i in stride(from: 1, to: 15, by: 2) {
+    print(i)
+}
+
+// while
+var cnt: Int = 0
+
+while cnt != 10 {
+    cnt += 1
+    print(cnt)
+    if cnt == 10 {
+        print("\(cnt) 끝")
+    }
+}
+
+cnt = 0
+
+repeat {
+    cnt += 1
+    print(cnt)
+    if cnt == 10 {
+        print("\(cnt) 끝!")
+    }
+    
+} while cnt != 10
+
+
+for i in 1...10 {
+    if i % 2 == 0 {
+        continue
+    }
+    print(i)
+}
+
+for i in 1...10 {
+    if i % 2 == 0 {
+        break
+    }
+    print(i)
+}
+
+for i in 2...9 {
+    for j in 1...9 {
+        print("\(i) * \(j) = \(i * j)")
+    }
+}
+
+var str: String = ""
+for _ in 1...5 {
+    str += "*"
+    print(str)
+}
+
+for i in 1...5 {
+    for j in 1...5 {
+        if j <= i {
+            print("*", terminator: "")
+        }
+    }
+    print()
+}
